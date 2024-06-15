62f80b1b94deb5fb2a4c889701e2f3eb63f101d7
Mark Justine Santos <114572934+MarkJustineSantos@users.noreply.github.com>
2024-06-15 09:33:06 +0800

    FIRST-COMMIT

---
+++ js/words.js
@@ -0,0 +1,83 @@
+let wordList = 
+[
+{
+word: "bayani",
+hint: "pambansang bayani ng Pilipinas"
+},
+{
+word: "martir",
+hint: "ipinapatay noong 1896"
+},
+{
+word: "manunulat",
+hint: "sumulat ng 'Noli Me Tangere'"
+},
+{
+word: "doktor",
+hint: "manggagamot ng mata"
+},
+{
+word: "makata",
+hint: "nagsulat ng mga tula"
+},
+{
+word: "polyglot",
+hint: "marunong ng maraming wika"
+},
+{
+word: "iskolar",
+hint: "magaling sa pag-aaral"
+},
+{
+word: "rebolusyonaryo",
+hint: "nag-udyok ng paglaban sa Kastila"
+},
+{
+word: "Noli Me Tangere",
+hint: "unang nobela ni Rizal"
+},
+{
+word: "El Filibusterismo",
+hint: "ikalawang nobela ni Rizal"
+},
+{
+word: "pilosopo",
+hint: "mahilig sa pilosopiya"
+},
+{
+word: "diarist",
+hint: "nagsusulat ng talaarawan"
+},
+{
+word: "kabataan",
+hint: "pinaglaanan ng mga gawa"
+},
+{
+word: "La Liga Filipina",
+hint: "samahang itinatag ni Rizal"
+},
+{
+word: "Calamba",
+hint: "lugar ng kapanganakan"
+},
+{
+word: "Propagandista",
+hint: "kasama sa kilusang propaganda"
+},
+{
+word: "espiritista",
+hint: "naniniwala sa espiritismo"
+},
+{
+word: "Jose",
+hint: "pangalan ni Rizal"
+},
+{
+word: "Realonda",
+hint: "apelyido ng ina"
+},
+{
+word: "Blumentritt",
+hint: "kaibigang Austriano ni Rizal"
+}
+]
